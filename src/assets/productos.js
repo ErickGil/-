33718ec0683f6bad18f1ff5a54949823323d2af0
ErickGil <ericksebagill@gmail.com>
2022-10-02 19:43:34 -0300

    se cambia mayuscula

--- src/assets/productos.js
+++ src/assets/productos.js
@@ -1,22 +1,22 @@
-import hieloGrande from "./img/bolsa10Kg.png";
-import hieloChico from "./img/bolsa2.5Kg.png";
-import barra from "./img/unaBarra.png";
+import HieloGrande from "./img/bolsa10Kg.png";
+import HieloChico from "./img/bolsa2.5Kg.png";
+import Barra from "./img/unaBarra.png";
 import jugo from "./img/jugoDeNaranja.png";
-import agua from "./img/bidonDeAgua.png";
+import Agua from "./img/bidonDeAgua.png";
 export const products = [{
     id:1, 
     name: "Bolsa de hielo de 10kg", 
     description: "Bolsa de hielo por 10kg en forma de dedal", 
     price:1800, 
     stock:40, 
-    image:hieloGrande, 
+    image:HieloGrande, 
     category: "bolsas"},
     {id:2, 
     name: "Bidon de Agua", 
     description: "Bidon de agua purificada envasada de 6 litros", 
     price:2800, 
     stock:20, 
-    image:agua, 
+    image:Agua, 
     category: "bidones"},
     {id:3, 
         name: "Bidon de Jugo", 
@@ -30,14 +30,14 @@ export const products = [{
         description: "Bolsa de hielo por 2,5kg en forma de dedal", 
         price:1200, 
         stock:50, 
-        image:hieloChico, 
+        image:HieloChico, 
         category: "bolsas"},
     {id:5, 
         name: "Barra de hielo", 
         description: "Barra compacta de hielo en 2 bolsa de 10kg cada una", 
         price:1600, 
         stock:21, 
-        image:barra, 
+        image:Barra, 
         category: "barras"}
     ]
 
